452f8f4e6ace62479c9d6272466ea35e356c5d43
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-11-05 11:29:55 -0300

    install vite/taliwind - h1

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "./sr/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
