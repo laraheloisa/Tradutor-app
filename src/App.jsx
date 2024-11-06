0eb7f0dd374be07a325896bb587616c75e836ef2
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-11-06 11:32:50 -0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { useState } from 'react'
 function App() {
   return (
     <>
-      <header className='bg-white shandow-sm'>
-        <div>
-          <h1>Tradutor</h1>
-        </div>
+      <header className='bg-white shadow-sm'>
+        <div className='max-w-5xl mx-auto px-4 py-3 flex items-center'>
+          <h1 className='text-headerColor '>Tradutor</h1>
+        </div> 
       </header>
       
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// @type {import('tailwindcss').Config} *//
+module.exports = {
   content: [
-    "index.html",
-    "./sr/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerColor: '#5f6368',
+        background1: '#f1f3f4',
+        background2: 'f5f5f5',
+        textColor: '#3c4043',
+
+      }
+    },
   },
   plugins: [],
 }
