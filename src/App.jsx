376464016dab810c9ab5ddddcccb8c62e8ce8e76
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-11-06 10:51:04 -0300

    dw

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <header className='bg-white shandow-sm'>
+        <div>
+          <h1>Tradutor</h1>
+        </div>
+      </header>
+      
+    </>
+  )
+}
+
+export default App
