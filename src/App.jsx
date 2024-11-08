4c7098164732f2f3761c796232bc5eb93b1f98f4
Lara Deitos <130068472+laraheloisa@users.noreply.github.com>
2024-11-08 11:39:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { useState } from 'react'
 
 function App() {
   return (
-    <>
+    <div className='min-h-screen bg-background flex flex-center '>
+
+
       <header className='bg-white shadow-sm'>
         <div className='max-w-5xl mx-auto px-4 py-3 flex items-center'>
-          <h1 className='text-headerColor '>Tradutor</h1>
+          <h1 className='text-headerColor text-2xl font-bold'>Tradutor</h1>
         </div> 
       </header>
+      <div>
+
+      </div>
       
-    </>
+    </div>
   )
 }
 
