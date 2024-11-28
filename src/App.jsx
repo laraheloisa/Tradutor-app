92c6dc04c9cc63106690066613252c2213c2bba6
Lara Deitos <130068472+laraheloisa@users.noreply.github.com>
2024-11-28 17:32:07 -0300

    isLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const languages = [
   {code: 'en', name:'Inglês'},
@@ -11,6 +11,18 @@ const languages = [
 ]
 
 function App() {
+  const [sourceLang, setSorceLang] = useState('pt')
+  const [targetLang, SetSorceLang] = useState('en')
+  const [sourceText, setSorceText] = useState(' ')
+  const [isLoading, setIsLoading] = useState('false')
+
+  useEffect(() => {
+
+
+
+  },  [sourceText])
+
+
   return (
     <div className='min-h-screen bg-background1 flex flex-col'>
       <header className='bg-white shadow-sm'>
@@ -21,7 +33,7 @@ function App() {
       <main className='flex-grow flex items-start justify-center px-4 py-8'>
         <div className='w-full max-w-5xl bg-white rounded-md shadow-sm overflow-hidden'>
           <div className='flex items-center justify-between p-4 border-b border-gray-200'>
-            <select className='text-sm text-textColor bg-transparent focus:outline-none cursor-pointer'> //o usuário pode escolher uma opção
+            <select value={sourceLang} onChange={ event =>  setSorceLang (event.target.value)} className='text-sm text-textColor bg-transparent focus:outline-none cursor-pointer'> //o usuário pode escolher uma opção
                 {languages.map((lang) => ( //Para cada elemento (que é um objeto) dentro de languages, o map() passará o item atual para a variável lang.  chave para identificar a forma única de cada item. E o value para retornar o valor que será enviado quando o usuário selecionar essa opção. 
                   <option key={lang.code} value={lang.code}> 
                     {lang.name}
@@ -46,7 +58,7 @@ function App() {
                 </svg>
             </button>
 
-            <select className='text-sm text-textColor bg-transparent focus:outline-none cursor-pointer'> //o usuário pode escolher uma opção
+            <select value={targetLang} onChange={ event =>  setSorceLang (event.target.value)} className='text-sm text-textColor bg-transparent focus:outline-none cursor-pointer'> //o usuário pode escolher uma opção
                 {languages.map((lang) => ( //Para cada elemento (que é um objeto) dentro de languages, o map() passará o item atual para a variável lang.  chave para identificar a forma única de cada item. E o value para retornar o valor que será enviado quando o usuário selecionar essa opção. 
                   <option key={lang.code} value={lang.code}> 
                     {lang.name}
@@ -60,6 +72,8 @@ function App() {
               <div className='p-4'> 
               {/* por padrão ele tem uma coluna, telas grandes/médias dois colunas */}
                 <textarea 
+                value={sourceText}
+                onChange={event =>  setSorceText (event.target.value)}
                 placeholder="Digite seu texto..." 
                 className='w-full h-40 text-lg text-textColor bg-tranparent resize-none border-none'>
                 </textarea>
@@ -67,9 +81,11 @@ function App() {
              
               <div className='p-4 relative bg-background1 border-1 border-gray-200'>
                 <div className='absolute inset-0 flex items-center justify-center'>
+
+
+                    
+                    
                     <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500'> 
-
-
                     <p className='text-lg text-textColor'></p>
                     </div>
                 </div>
